eae40277cfd088ec3e2689df6ae10b1bc27c3c29
johngibbs15 <john.gibbs901@gmail.com>
2023-04-04 12:46:26 -0400

    Styling

--- src/EventList.js
+++ src/EventList.js
@@ -7,23 +7,25 @@ class EventList extends Component {
     render() {
         const { events } = this.props;
         return (
-            <div
-                style={{
-                    width: '100vw',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
+            <>
                 <h2>Events</h2>
-                <ul className="EventList">
-                    {events.map((event) => (
-                        <li key={event.id}>
-                            <Event event={event} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <div
+                    style={{
+                        width: '100vw',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <ul className="EventList">
+                        {events.map((event) => (
+                            <li key={event.id}>
+                                <Event event={event} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </>
         );
     }
 }
